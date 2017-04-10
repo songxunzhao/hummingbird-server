cdcaed9cd0321f82e8856d98046b81806194e90d
Peter Lejeck <peter.lejeck@gmail.com>
2017-04-09 19:48:13 -0700

    lol will this work

--- db/migrate/20170326222205_add_time_spent_to_library_entry.rb
+++ db/migrate/20170326222205_add_time_spent_to_library_entry.rb
@@ -8,7 +8,7 @@ class AddTimeSpentToLibraryEntry < ActiveRecord::Migration
   def up
     say_with_time 'Filling time_spent column' do
       LibraryEntry.by_kind(:anime).joins(:anime)
-        .update_in_batches('time_spent = progress * anime.episode_length')
+        .update_in_batches('time_spent = progress * (SELECT episode_length FROM anime WHERE anime.id = library_entries.anime_id)')
     end
 
     change_column_default :library_entries, :time_spent, 0
